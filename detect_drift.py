770369bb7a527817d99bf7e3a5aeec754f9e4176
Thomas <thom.montet@gmail.com>
2023-03-14 11:23:45 +0100

    add final benchmark

--- detect_drift.py
+++ detect_drift.py
@@ -1,8 +1,10 @@
 from collections import Counter
 from scipy.spatial.distance import jensenshannon
+from scipy.stats import ks_2samp, wasserstein_distance, chi2_contingency
 from river import drift
 import numpy as np
 import pandas as pd
+import time
 
 
 class DetectDrift:
@@ -20,6 +22,18 @@ class DetectDrift:
 
         return [probabilites_1, probabilites_2]
 
+    def detect_drift(self, value):
+        return (value > 0.1) if self.test == 'js' else \
+            (value < 0.05) if self.test == 'ks' else \
+            (value > 0.1) if self.test == 'wd' else \
+            (value < 0.05) if self.test == 'chi2' else \
+            ValueError('Invalid test specified')
+    
+    def switch_method(self, ref, new):
+        if self.test == "ks":
+            ks_stat, p_value = ks_2samp(ref, new)
+            return p_value
+
     def method_drift(self, dfs):
         benchmark_prob_detail = []
         benchmark_prob        = []
@@ -28,12 +42,26 @@ class DetectDrift:
             df_ref = dataset['Data'][0]
             df_new = dataset['Data'][1]
             all_correct = True
+            total_time  = 0
             
             for column in df_new.columns:
                 correct_column     = True
                 probabilites       = self.get_proba(df_ref[column], df_new[column])
-                drift_probability  = jensenshannon(probabilites[0], probabilites[1])
+                
+                switcher = {
+                    'js': jensenshannon(probabilites[0], probabilites[1]),
+                    'wd': wasserstein_distance(probabilites[0], probabilites[1]),
+                    'ks': self.switch_method(probabilites[0], probabilites[1]),
+                    'default': jensenshannon(probabilites[0], probabilites[1])
+                }
+                
+                start_time = time.time()
+                drift_probability  = switcher.get(self.test, jensenshannon(probabilites[0], probabilites[1]))
+                elapsed_time = time.time() - start_time
+                total_time  += elapsed_time
+                
                 is_drift           = self.detect_drift(drift_probability)
+                
                 
                 if(df_new[column].dtype == np.float64 or df_new[column].dtype == np.int64):
                     category    = "numeric"
@@ -49,21 +77,14 @@ class DetectDrift:
                         correct_column = False
                         all_correct    = False
 
-                benchmark_prob_detail.append({'Df': dataset['Stats'], 'column': column, 'type': category, 'js': round(drift_probability, 3), 'drift_js': is_drift, 'correct': correct_column})
+                benchmark_prob_detail.append({'Df': dataset['Stats'], 'column': column, 'type': category, self.test: round(drift_probability, 3), 'drift_detected': is_drift, 'correct': correct_column, 'time': elapsed_time})
         
-            benchmark_prob.append({'Num_Cat': dataset['Num_Cat'], 'Num_Number': dataset['Num_Number'], 'Right': all_correct})
+            benchmark_prob.append({'Num_Cat': dataset['Num_Cat'], 'Num_Number': dataset['Num_Number'], 'Right': all_correct, 'Time': total_time})
         
         df_benchmark_pob        = pd.DataFrame(benchmark_prob)
         df_benchmark_pob_detail = pd.DataFrame(benchmark_prob_detail)
         return df_benchmark_pob_detail, df_benchmark_pob
-        
-    def detect_drift(self, value):
-        return (value > 0.1) if self.test == 'js' else \
-            (value <= 0.05) if self.test == 'ks' else \
-            (value > 0.1) if self.test == 'wd' else \
-            (value < 0.05) if self.test == 'chi2' else \
-            ValueError('Invalid test specified')
-            
+
     def adwin(self, df):
         
         adwin = drift.ADWIN()
@@ -74,4 +95,21 @@ class DetectDrift:
             if adwin.drift_detected:
                 print(f"Change detected at index {i}, input value: {val}")
         
-        
+    def final_benchmark(self, all_benchmark):
+        final_benchmark = []
+
+        for i in range(len(all_benchmark)):
+            result    = all_benchmark[i]["Result"][1]
+            method    = all_benchmark[i]["Test"]
+            counts    = result["Right"].value_counts()
+            mean_time = result['Time'].mean()
+
+            try:
+                ratio  = counts[True] / counts.sum()
+            except:
+                ratio  = 0
+                
+            final_benchmark.append({'Method': method, 'Correct_Ratio': round(ratio, 3), 'Avg_Time': mean_time})
+
+        df_final_benchmark = pd.DataFrame(final_benchmark)
+        return df_final_benchmark
